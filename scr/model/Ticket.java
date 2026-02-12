6fcfa5794194ba68a8537ee76b29f59b5242f991
luciana.cosentino <luciana.cosentino@fing.edu.uy>
2026-02-12 09:32:40 -0300

    declaraciones en archivo equivocado

---
+++ scr/model/Ticket.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ticket {
+
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private EstadoTicket estado;
+    private Prioridad prioridad;
+    private List<String> historial;
+
+    public Ticket(int id, String titulo, String descripcion, Prioridad prioridad) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.estado = EstadoTicket.ABIERTO;
+        this.historial = new ArrayList<>();
+        historial.add("Ticket creado en estado ABIERTO");
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public EstadoTicket getEstado() {
+        return estado;
+    }
+
